057f3cbe23ac19833a96752237e47a0a768320ff
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2015-02-01 21:49:23 -0500

    Modified script to get summoner match history

--- Random_Summoner_Generator/Get_Summoner_Data.py
+++ Random_Summoner_Generator/Get_Summoner_Data.py
@@ -2,15 +2,17 @@ __author__ = 'Deniz'
 
 from RiotWatcher.riotwatcher import RiotWatcher
 from RiotWatcher.riotwatcher import LoLException
-import re, argparse
+import re, argparse, os
 
 # Setup RiotWatcher object with api key
 api = RiotWatcher('17dd8043-3e16-4cad-a388-985bfd93d275')
 
 summoner_data = []
+summoner_ids = []
 
 def main():
     global summoner_data
+    global summoner_ids
 
     parser = argparse.ArgumentParser(description='Attempt to grab data for given list of summoners.')
 
@@ -43,14 +45,18 @@ def get_summoner_data(summoners_list):
 
         # Try to retrieved the summoner data for the ID we just got
         try:
-             s_data = api.get_ranked_stats(tmp_id, region=None, season=None)
+             s_data = api._match_history_request(tmp_id+"?beginIndex=15&endIndex=30", region=None)
+             # If we get data back, append the ID to array and data to another
+             summoner_ids.append(tmp_id)
              summoner_data.append(s_data)
         except LoLException:
             print 'DATA NOT FOUND FOR SUMMONER ' + str(tmp_id)
 
+# Write the summoner data, writing a new file for each summoner
 def write_summoner_data(inputLocation):
-    with open("_out_"+inputLocation+".txt", 'w') as file:
-        for summoner in summoner_data:
+    for summoner in summoner_data:
+        for id in summoner_ids:
+            file = open(os.curdir+"\_out2\_"+id+"_"+inputLocation+".txt", 'w')
             file.writelines(str(summoner)+"\n\n\n")
 
 if __name__ == "__main__":
